dfd2c34667e3a631db58a0736b4d19b65de9d65e
JHBR <jhbr@datarespons.no>
2017-09-08 13:04:43 +0200

    Replace scan of static image with webcam capture with OpenCV

--- py/readQR.py
+++ py/readQR.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
+import cv2
 from PIL import Image
 import zbarlight
 
-filePath = './qrcode.png'
-with open(filePath, 'rb') as imageFile:
-    image = Image.open(imageFile)
-    image.load()
 
-qrCode = zbarlight.scan_codes('qrcode', image)
-print('QR code: %s' % qrCode)
+cv2.namedWindow('QR Camera')
+cap = cv2.VideoCapture(0) # first indexed webcam
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Create grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Build image and scan for QR code
+    image = Image.fromarray(gray)
+    qrcode = zbarlight.scan_codes('qrcode', image)
+
+    # Display resulting frame and print any QR code
+    cv2.imshow('QR Camera', frame)
+    if qrcode:
+        print(qrcode)
+
+    # Exit on keypress 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
