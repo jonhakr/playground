baf8fe91a8e7cdd5ebad0dcf91fc1490efc361f6
JHBR <jhbr@datarespons.no>
2017-10-18 13:30:57 +0200

    Revert second wait to edge detect and update readme. Reliability issues were caused by bad 3.5 mm jack button, not SW

--- py/rpiShutdownDaemon/shutdown_d.py
+++ py/rpiShutdownDaemon/shutdown_d.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import subprocess
-import time
 
-LONG_PRESS_S = 3.0
+LONG_PRESS_MS = 3000
 GPIO.setmode(GPIO.BOARD) # Use pin numbering
 WAKE_UP_CH = 5 # Pin 05 (GPIO 03)
 GPIO.setup(WAKE_UP_CH, GPIO.IN) 
@@ -11,16 +10,13 @@ GPIO.setup(WAKE_UP_CH, GPIO.IN)
 
 # Shutdown/reboot system when wake up pin is shorted to GND
 GPIO.wait_for_edge(WAKE_UP_CH, GPIO.FALLING)
-start = time.time()
-longPress = True
-while (time.time() - start) < LONG_PRESS_S:
-    if GPIO.input(WAKE_UP_CH) == GPIO.HIGH:
-        longPress = False
-        break
-    time.sleep(0.1)
-if longPress:
+#print("Button pressed")
+ch = GPIO.wait_for_edge(WAKE_UP_CH, GPIO.RISING, timeout=LONG_PRESS_MS)
+if ch is None:
+    #print("Long press")
     cmd = "sudo reboot"
 else:
+    #print("Short press")
     cmd = "sudo shutdown -h now"
 
 subprocess.call(cmd,
